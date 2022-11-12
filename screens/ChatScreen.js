b9994b4938c4c93c7e8a131aac0f5e8e8b189ed0
ongboonhan <edvinongbh@gmail.com>
2022-11-12 15:02:54 +0800

    add a header logout icon

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -3,6 +3,8 @@ import { Button, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { auth } from "../firebase";
 import { onAuthStateChanged, signOut } from "firebase/auth";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function ChatScreen() {
   const navigation = useNavigation();
@@ -13,11 +15,15 @@ export default function ChatScreen() {
       if (user) navigation.navigate("Chat", { id: user.id, email: user.email });
       else navigation.navigate("Login");
     });
+
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={logout}>
+          <MaterialCommunityIcons name="logout" size={30} color="grey" />
+        </TouchableOpacity>
+      ),
+    });
   }, []);
   const logout = () => signOut(auth);
-  return (
-    <View>
-      <Button onPress={logout} title="Logout" />
-    </View>
-  );
+  return <View></View>;
 }
