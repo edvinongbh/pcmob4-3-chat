e6f3bfe0d9087f9e27085fce2581650424646502
ongboonhan <edvinongbh@gmail.com>
2022-11-12 15:55:57 +0800

    connect to firebase

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,26 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { GiftedChat } from "react-native-gifted-chat";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
 
-const demoMessage = {
-  _id: 1,
-  text: "Hello there!",
-  createdAt: new Date(),
-  user: {
-    _id: 2,
-    name: "Demo person",
-    avatar: "https://placeimg.com/140/140/any",
-  },
-};
+const messageCollection = collection(db, "messages");
 
 export default function ChatScreen() {
   const [messages, setMessages] = useState([]);
   const navigation = useNavigation();
+
   useEffect(() => {
+    const unsubscribe = onSnapshot(messageCollection, (collectionSnapshot) => {
+      const messages = collectionSnapshot.docs.map((doc) => {
+        const date = doc.data().createdAt.toDate();
+        const newDoc = { ...doc.data(), createdAt: date };
+        return newDoc;
+      });
+      setMessages(messages);
+    });
+
     // if logged in, user is true-thy
     // of not logging in, user is false-y
     onAuthStateChanged(auth, (user) => {
@@ -36,14 +38,14 @@ export default function ChatScreen() {
       ),
     });
 
-    setMessages([demoMessage]);
+    return unsubscribe;
   }, []);
 
   const logout = () => signOut(auth);
 
   function sendMessages(newMessages) {
     console.log(newMessages);
-    setMessages([...messages, ...newMessages]);
+    addDoc(messageCollection, newMessages[0]);
   }
 
   return (
