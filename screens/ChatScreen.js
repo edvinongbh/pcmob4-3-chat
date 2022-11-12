4377b331d343fd30890babb15330f0c428e7a3ac
ongboonhan <edvinongbh@gmail.com>
2022-11-12 15:36:47 +0800

    Set up a demo chat

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,12 +1,24 @@
-import React, { useEffect } from "react";
-import { Button, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { auth } from "../firebase";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { GiftedChat } from "react-native-gifted-chat";
+
+const demoMessage = {
+  _id: 1,
+  text: "Hello there!",
+  createdAt: new Date(),
+  user: {
+    _id: 2,
+    name: "Demo person",
+    avatar: "https://placeimg.com/140/140/any",
+  },
+};
 
 export default function ChatScreen() {
+  const [messages, setMessages] = useState([]);
   const navigation = useNavigation();
   useEffect(() => {
     // if logged in, user is true-thy
@@ -23,7 +35,23 @@ export default function ChatScreen() {
         </TouchableOpacity>
       ),
     });
+
+    setMessages([demoMessage]);
   }, []);
+
   const logout = () => signOut(auth);
-  return <View></View>;
+
+  function sendMessages(newMessages) {
+    console.log(newMessages);
+    setMessages([...messages, ...newMessages]);
+  }
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={sendMessages}
+      listViewProps={{ style: { backgroundColor: "grey" } }}
+      user={{ _id: 1 }}
+    />
+  );
 }
